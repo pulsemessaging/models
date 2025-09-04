d8363fa393fd98cc2682b1950011705808cade85
Luciism <lucism.dev@gmail.com>
2025-09-05 06:01:21 +1200

    feat: add pagination ListQuery model

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ pub mod chats;
 pub mod messages;
 pub mod credentials;
 pub mod sessions;
+pub mod query;
 
 mod enums;
 
---
+++ src/query.rs
@@ -0,0 +1,23 @@
+use serde::Deserialize;
+use time::OffsetDateTime;
+
+#[cfg(feature = "validate")]
+use validator::Validate;
+
+fn default_before() -> OffsetDateTime {
+    OffsetDateTime::now_utc()
+}
+fn default_limit() -> i64 {
+    25
+}
+
+#[cfg_attr(feature = "validate", derive(validator::Validate))]
+#[derive(Deserialize, Debug)]
+pub struct ListQuery {
+    #[serde(with = "time::serde::rfc3339")]
+    #[serde(default = "default_before")]
+    pub before: OffsetDateTime,
+    #[serde(default = "default_limit")]
+    #[cfg_attr(feature = "validate", validate(min = 0, max = 200))]
+    pub limit: i64,
+}
